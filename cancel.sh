e277ec9f0b73977df173ba49e5492c226520142a
Sevelantis <hamer223@o2.pl>
2023-05-24 15:55:14 +0100

    added mysql

--- cancel.sh
+++ cancel.sh
@@ -1,5 +1,22 @@
-#! /bin/sh -eux
+#! /bin/sh 
+NAMESPACE=egs-conv
 
+# delete secrets
+kubectl delete secret activemq-secrets -n egs-conv 
+kubectl delete secret auth0-secrets -n egs-conv
+kubectl delete secret composer-secrets -n egs-conv 
+kubectl delete secret localstack-secrets -n egs-conv 
+kubectl delete secret mysql-secrets -n egs-conv 
 
+# delete configmaps
+kubectl delete configmap activemq-configmap -n egs-conv 
+kubectl delete configmap auth0-configmap -n egs-conv 
+kubectl delete configmap composer-configmap -n egs-conv 
+kubectl delete configmap conversion-configmap -n egs-conv 
+kubectl delete configmap localstack-configmap -n egs-conv 
+kubectl delete configmap mysql-configmap -n egs-conv 
+kubectl delete configmap s3connector-configmap -n egs-conv 
+
+# delete deploy
 kubectl delete -f infra.yaml
-kubectl delete -f deployment.yaml
+# kubectl delete -f mvp.yaml
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,24 @@
 #! /bin/sh -eux
+NAMESPACE=egs-conv
+
+# load secrets
+kubectl create secret generic activemq-secrets --from-env-file=activemq/activemq-secrets.env -n ${NAMESPACE}
+kubectl create secret generic auth0-secrets --from-env-file=auth0/auth0-secrets.env -n ${NAMESPACE}
+kubectl create secret generic composer-secrets --from-env-file=composer/composer-secrets.env -n ${NAMESPACE}
+kubectl create secret generic localstack-secrets --from-env-file=localstack/localstack-secrets.env -n ${NAMESPACE}
+kubectl create secret generic mysql-secrets --from-env-file=mysql/mysql-secrets.env -n ${NAMESPACE}
+
+# load configmaps
+kubectl create configmap activemq-configmap --from-env-file=activemq/activemq-configmap.env -n ${NAMESPACE}
+kubectl create configmap auth0-configmap --from-env-file=auth0/auth0-configmap.env -n ${NAMESPACE}
+kubectl create configmap composer-configmap --from-env-file=composer/composer-configmap.env -n ${NAMESPACE}
+kubectl create configmap conversion-configmap --from-env-file=conversion/conversion-configmap.env -n ${NAMESPACE}
+kubectl create configmap localstack-configmap --from-env-file=localstack/localstack-configmap.env -n ${NAMESPACE}
+kubectl create configmap mysql-configmap --from-env-file=mysql/mysql-configmap.env -n ${NAMESPACE}
+# kubectl create configmap mysql-init-configmap --from-file mysql/config.sql -n ${NAMESPACE}
+kubectl create configmap s3connector-configmap --from-env-file=s3connector/s3connector-configmap.env -n ${NAMESPACE}
 
 
+# apply deploy
 kubectl apply -f infra.yaml
-kubectl apply -f deployment.yaml
+# kubectl apply -f mvp.yaml
